da2730237423136abefc96da7b1b7ce4d55ad81e
Darren Hobin <darrenhobin@live.com>
2018-01-10 23:33:56 -0500

    Fix animation on welcome screen - DO NOT MERGE
    ** DO NOT MERGE, SEE BELOW **
    
    Removes most jQuery as it conflicts with node. Ultimately, this fix
    needs to be modified to properly remove all jQuery in the best way
    possible. Right now it uses methods like querySelector, which is
    unnecessarly slow. Further, there is like more jQuery code that must be
    replaced with plain ol' Javascript.

--- js/index.js
+++ js/index.js
@@ -1,51 +1,46 @@
 var tools = {
   getLineLength: function(el){
-    var x1 = el.attr('x1'), x2 = el.attr('x2'), y1 = el.attr('y1'), y2 = el.attr('y2');
+    var x1 = el.getAttribute('x1'), x2 = el.getAttribute('x2'), y1 = el.getAttribute('y1'), y2 = el.getAttribute('y2');
     return Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1), 2));
   },
   getCircleLength: function(el){
-    var radius = parseInt(el.attr('r'));
+    var radius = parseInt(el.getAttribute('r'));
     return 2 * Math.PI * radius;
   },
   setDashStyles: function(el, i, addDelay){
-    var type = el.get(0).nodeName;
+    var type = el.tagName;
     var len = 0;
     var duration = .4;
     var delay = i * (duration - .1) + addDelay;
+    console.log(i);
     if(type === 'line'){
       len = this.getLineLength(el) + 1;
     }else if(type === 'circle'){
       len = this.getCircleLength(el) + 1;
     }else if(type === 'path'){
-      len = el.get(0).getTotalLength() + 1;
+      len = el.getTotalLength() + 1;
     }
-    
-    el.css({
-      strokeDasharray:len, 
-      strokeDashoffset:len, 
-      animation: 'undash '+duration+'s '+delay+'s forwards'
-    });
+
+    el.style.strokeDasharray = len;
+    el.style.strokeDashoffset = len;
+    el.style.animation = 'undash '+duration+'s '+delay+'s forwards';
   }
 };
+ console.log("foo");
 
+// document.querySelectorAll('.middle').forEach(function(el, i){
+//   tools.setDashStyles(el, parseInt(el.data('order')), 0);
+// });
 
-$('.middle').each(function(i, el){
-  tools.setDashStyles($(el), parseInt($(el).data('order')), 0);
-});
-
- $('.top').each(function(i, el){
-   tools.setDashStyles($(el), i, 0);
+ document.querySelectorAll('.top').forEach(function(el, i){
+   tools.setDashStyles(el, i, 0);
  });
 
-  $('.bottom').each(function(i, el){
-   tools.setDashStyles($(el), i, 0);
+  document.querySelectorAll('.bottom').forEach(function(el, i){
+   tools.setDashStyles(el, i, 0);
  });
 
 
-$('.low-mid').each(function(i, el){
-  tools.setDashStyles($(el), i, 3);
+document.querySelectorAll('.low-mid').forEach(function(el, i){
+  tools.setDashStyles(el, i, 3);
 });
-
-setTimeout(function(){
-  $('.social-icon').fadeIn();
-}, 4000)
