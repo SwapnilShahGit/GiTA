a9d662cdf7da5facae43b5d26989ecf35435b205
Rajdeep Nanua <rajdeep.nanua@mail.utoronto.ca>
2018-02-04 20:14:27 -0500

    Added a user input check for git force commands

--- main.js
+++ main.js
@@ -16,11 +16,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 700, height: 600, frame: false, resizable: false})
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'terminal.html'),
     protocol: 'file:',
     slashes: true
   }))
